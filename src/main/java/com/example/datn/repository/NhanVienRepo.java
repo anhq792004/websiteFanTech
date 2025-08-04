8a2729a4760f1f46367d4e2fc8bb4d0c8790728c
Khoi <khoinaph31928@fpt.edu.vn>
2025-08-04 22:18:44 +0700

    web

--- src/main/java/com/example/datn/repository/NhanVienRepo.java
+++ src/main/java/com/example/datn/repository/NhanVienRepo.java
@@ -2,11 +2,14 @@ package com.example.datn.repository;
 
 import com.example.datn.entity.KhachHang;
 import com.example.datn.entity.NhanVien.NhanVien;
+import com.example.datn.entity.TaiKhoan;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface NhanVienRepo extends JpaRepository<NhanVien,Long> {
@@ -24,5 +27,7 @@ public interface NhanVienRepo extends JpaRepository<NhanVien,Long> {
 //            "OR LOWER(kh.taiKhoan.email) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
             "OR LOWER(nv.soDienThoai) LIKE LOWER(CONCAT('%', :keyword, '%')))")
     Page<NhanVien> searchNhanVienKhongCoTrangThai(String keyword, Pageable pageable);
+
+    Optional<NhanVien> findByTaiKhoan(TaiKhoan taiKhoan);
 
 }
--- src/main/java/com/example/datn/repository/TaiKhoanRepo.java
+++ src/main/java/com/example/datn/repository/TaiKhoanRepo.java
@@ -4,9 +4,10 @@ import com.example.datn.entity.TaiKhoan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TaiKhoanRepo extends JpaRepository<TaiKhoan,Long> {
     TaiKhoan findByEmail(String email);
 
-    boolean existsByEmail(String email);
 }
